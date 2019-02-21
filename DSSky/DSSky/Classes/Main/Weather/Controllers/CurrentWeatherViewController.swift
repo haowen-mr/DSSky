37b2b8ab43b63e700685ca8e30761e99ac47d804
左得胜 <zuodesheng@qq.com>
2019-02-21 17:53:47 +0800

    添加控制器

---
+++ DSSky/DSSky/Classes/Main/Weather/Controllers/CurrentWeatherViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CurrentWeatherViewController.swift
+//  DSSky
+//
+//  Created by 左得胜 on 2019/2/21.
+//  Copyright © 2019 左得胜. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherViewController: WeatherBaseViewController {
+    // MARK: - Property
+    // MARK: Public
+    
+    
+    // MARK: Private
+    
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    
+    // MARK: - Action
+    
+}
+
+// MARK: - Private Method
+private extension CurrentWeatherViewController {
+    func setupUI() {
+        
+    }
+}
---
+++ DSSky/DSSky/Classes/Main/Weather/Controllers/WeekWeatherViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WeekWeatherViewController.swift
+//  DSSky
+//
+//  Created by 左得胜 on 2019/2/21.
+//  Copyright © 2019 左得胜. All rights reserved.
+//
+
+import UIKit
+
+class WeekWeatherViewController: WeatherBaseViewController {
+    // MARK: - Property
+    // MARK: Public
+    
+    
+    // MARK: Private
+    
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    
+    // MARK: - Action
+    
+}
+
+// MARK: - Private Method
+private extension WeekWeatherViewController {
+    func setupUI() {
+        
+    }
+}
